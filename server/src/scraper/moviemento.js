bb6d363979d15c8545fbf92b1b98583e91748c9d
Jason Anderson <diurnalist@gmail.com>
2016-12-26 18:33:03 -0800

    Add Moviemento scraper
    This re-uses the earlier work for Il Kino, using the Kinoheld website.

--- server/src/scraper/moviemento.js
+++ server/src/scraper/moviemento.js
@@ -1,1 +1,56 @@
-module.exports = (callback) => callback(null, []);
+const log = require('debug')('scraper:moviemento');
+const request = require('request');
+const split = require('split');
+const url = require('url');
+
+const dataExtractRegex = /dataLayer\.push\((.*?)\);$/;
+const host = 'https://www.kinoheld.de';
+
+function getViewData() {
+  return new Promise((resolve, reject) => {
+    request({
+      url: url.resolve(host, 'kino-berlin/moviemento-kino'),
+      qs: {
+        layout: 'shows'
+      }
+    })
+    .pipe(split())
+    .on('data', (line) => {
+      const [, json] = line.match(dataExtractRegex) || []
+
+      if (json) {
+        resolve(JSON.parse(json));
+      }
+    })
+    .on('error', reject);
+  });
+}
+
+module.exports = (callback) => {
+  log('starting');
+  getViewData()
+    .then(({ shows, movies }) => {
+      const showtimes = [];
+
+      Object.keys(shows).forEach((key) => {
+        const show = shows[key];
+        const movie = movies[show.movieId];
+
+        const deepLink = url.resolve(host, show.url);
+        const language = show.flags[0];
+        const showtime = new Date(show.start);
+        const title = movie.name.replace(/\((ov|ome?u)\)/i, '').trim();
+
+        showtimes.push({
+          deepLink,
+          language,
+          location: 'Moviemento',
+          showtime,
+          title
+        });
+      });
+
+      log(`got ${showtimes.length} results`);
+      callback(null, showtimes);
+    }, callback);
+};
